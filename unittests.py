2b6791db9cec61664d0fb20721a0e03c94dd9299
Ian Havelock <havelock.needkey@gmail.com>
2014-01-20 11:31:59 -0500

    Added multiple unittests

--- unittests.py
+++ unittests.py
@@ -3,9 +3,33 @@ import unittest
 
 class SurrealismUnittests(unittest.TestCase):
 
-    def test_getsentence_returns_a_string(self):
-        _sentence = s.getsentence()
-        print _sentence
-        print str(type(_sentence))
-        var_types = (str, unicode)
-        self.assertIsInstance(_sentence, var_types)
+
+    def test_getsentence_returns_a_unicode_string(self):
+        sentence = s.getsentence()
+        self.assertEqual(type(sentence), unicode)
+        
+        
+    def test_getfault_returns_a_unicode_string(self):
+        fault = s.getfault()
+        self.assertEqual(type(fault), unicode)
+        
+        
+    def test_getsentence_returns_a_unicode_string_with_integer_upper_limit(self):
+        limits = s._gettablelimits()
+        upper_limit = limits['sen_count']
+        sentence = s.getsentence(upper_limit)
+        self.assertEqual(type(sentence), unicode)
+        
+        
+    def test_getsentence_returns_a_unicode_string_with_integer_lower_limit(self):
+        lower_limit = 1
+        sentence = s.getsentence(lower_limit)
+        self.assertEqual(type(sentence), unicode)
+        
+    
+    def test_getsentence_returns_a_unicode_string_over_integer_upper_limit(self):
+        limits = s._gettablelimits()
+        over_limit = limits['sen_count'] + 1
+        #sentence = s.getsentence(over_limit)
+        self.assertRaises(AttributeError, sentence = s.getsentence(over_limit)
+        
