bf8d51fe561efc6a9885ca7bca71c48c579fa60c
Ian Havelock <havelock.needkey@gmail.com>
2014-01-20 10:16:26 -0500

    Unittest fix/tweak

--- unittests.py
+++ unittests.py
@@ -1,1 +1,11 @@
-import surrealism.__init__
+import surrealism as s
+import unittest
+
+class SurrealismUnittests(unittest.TestCase):
+
+    def test_getsentence_returns_a_string(self):
+        _sentence = s.getsentence()
+        print _sentence
+        print str(type(_sentence))
+        var_types = (str, unicode)
+        self.assertIsInstance(_sentence, var_types)
