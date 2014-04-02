3ee18cf0f8e9e45acefd77d2ef9596cb3f83c93a
Ian Havelock <ian@morrolan.com>
2014-04-02 16:51:06 +0100

    Pushed into new 'develop' branch.

--- surrealism/__init__.py
+++ surrealism/__init__.py
@@ -547,8 +547,6 @@ def __replace_random__(_sentence):
         return _sentence
 
 
-<<<<<<< HEAD
-=======
 def __replace_repeat__(_sentence):
     """
     HERE BE DRAGONS!
@@ -560,7 +558,6 @@ def __replace_repeat__(_sentence):
     pass
 
 
->>>>>>> origin/repeat-dev
 def __replace_capitalise__(_sentence):
     """here we replace all instances of #CAPITALISE and cap the next word.
     ############
