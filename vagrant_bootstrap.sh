2be4491d96df4694ac07300c5636663c08c42eb3
Ian Havelock <ian@morrolan.com>
2014-06-17 07:54:13 +0100

    Added working Vagrant setup.

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
 
+apt-get update
+apt-get install -y python2.7
+apt-get install -y python3.2
+apt-get install -y python-virtualenv
+
+mkdir /home/vagrant/surrealism27
+cd /home/vagrant
+virtualenv -p /usr/bin/python2.7 /home/vagrant/surrealism27
+cd /home/vagrant/surrealism27
+source /home/vagrant/surrealism27/bin/activate
+pip install nose
+pip install rednose
+
+
+mkdir /home/vagrant/surrealism32
