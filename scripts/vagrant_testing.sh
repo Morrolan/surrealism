1093a5b03cd5ad20ffea8f3fe221c55b08d29d50
Ian Havelock <ian@morrolan.com>
2014-06-26 12:26:11 +0100

    Travis exits with  error code of 1.

--- scripts/vagrant_testing.sh
+++ scripts/vagrant_testing.sh
@@ -5,7 +5,7 @@ source /home/vagrant/surrealism27/bin/activate
 sudo mkdir surrealism
 sudo git clone https://github.com/Morrolan/surrealism.git
 cd surrealism
-sudo git checkout develop
+#sudo git checkout develop
 sudo nosetests --rednose --with-xunit --xunit-file=/vagrant/results/nosetests2.7.xml /home/vagrant/surrealism27/surrealism/test_surrealism.py
 python /home/vagrant/surrealism27/surrealism/large_output_test.py
 deactivate
@@ -15,7 +15,7 @@ source /home/vagrant/surrealism32/bin/activate
 sudo mkdir surrealism
 sudo git clone https://github.com/Morrolan/surrealism.git
 cd surrealism
-sudo git checkout develop
+#sudo git checkout develop
 sudo nosetests --rednose --with-xunit --xunit-file=/vagrant/results/nosetests3.2.xml /home/vagrant/surrealism32/surrealism/test_surrealism.py
 python /home/vagrant/surrealism27/surrealism/large_output_test.py
 deactivate
