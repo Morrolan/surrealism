919c501da7cfb83cbf6458c6d743ed42139ec096
Ian Havelock <ian@morrolan.com>
2014-09-05 13:17:05 +0100

    Travis is choking when trying to test 3.2 and 3.3.  However this works perfectly fine if I test t myself.  The Attribute error received appears to mostly be caused by .pyc files hanging around, but in Travis I appear to have no control of this and can't delete them after the 2.7 testing?
    Travis_CI build 118

--- scripts/vagrant_testing.sh
+++ scripts/vagrant_testing.sh
@@ -6,7 +6,6 @@ sudo mkdir surrealism
 sudo git clone https://github.com/Morrolan/surrealism.git
 cd surrealism
 sudo git checkout develop
-sudo rm *.pyc
 sudo nosetests --rednose --with-xunit --xunit-file=/vagrant/results/nosetests2.7.xml /home/vagrant/surrealism27/surrealism/test_surrealism.py
 python /home/vagrant/surrealism27/surrealism/large_output_test.py
 deactivate
@@ -17,7 +16,6 @@ sudo mkdir surrealism
 sudo git clone https://github.com/Morrolan/surrealism.git
 cd surrealism
 sudo git checkout develop
-sudo rm *.pyc
 sudo nosetests --rednose --with-xunit --xunit-file=/vagrant/results/nosetests3.2.xml /home/vagrant/surrealism32/surrealism/test_surrealism.py
 python /home/vagrant/surrealism32/surrealism/large_output_test.py
 deactivate
